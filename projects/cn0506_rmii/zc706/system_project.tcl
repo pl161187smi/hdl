amaslov86/adi-hdl

Add cn0506_rmii/zc706 support on revB

projects/cn0506_rmii/zc706/system_project.tcl
@@ -1,0 +1,14 @@
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project cn0506_zc706
+adi_project_files  cn0506_zc706 [list \
+  "system_top.v" \
+  "system_constr.xdc"\
+  "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
+
+adi_project_run cn0506_zc706
+

pl161187smi/hdl

projects/cn0506_rmii/zc706/system_project.tcl
@@ -1,0 +1,14 @@
+
+source ../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+adi_project cn0506_rmii_zc706
+adi_project_files  cn0506_rmii_zc706 [list \
+  "system_top.v" \
+  "system_constr.xdc"\
+  "$ad_hdl_dir/projects/common/zc706/zc706_system_constr.xdc" \
+  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
+
+adi_project_run cn0506_rmii_zc706
+